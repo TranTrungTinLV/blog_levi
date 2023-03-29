90759aa5c42115b1950af135a03c137f76f4b23d
Aldor Tran <aldor.tran@gmail.com>
2023-03-30 02:51:17 +0700

    update1

--- src/pages/speaking.jsx
+++ src/pages/speaking.jsx
@@ -1,47 +1,78 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import clsx from 'clsx'
 
-import { Card } from '@/components/Card'
-import { Section } from '@/components/Section'
-import { SimpleLayout } from '@/components/SimpleLayout'
+import { Container } from '@/components/Container'
+import {
+    GitHubIcon,
+    InstagramIcon,
+    LinkedInIcon,
+    TwitterIcon,
+} from '@/components/SocialIcons'
+import sadImage from '@/images/sad.jpg'
 
-function SpeakingSection({ children, ...props }) {
-  return (
-    <Section {...props}>
-      <div className="space-y-16">{children}</div>
-    </Section>
-  )
+function SocialLink({ className, href, children, icon: Icon }) {
+    return (
+        <li className={clsx(className, 'flex')}>
+            <Link
+                href={href}
+                className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
+            >
+                <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500" />
+                <span className="ml-4">{children}</span>
+            </Link>
+        </li>
+    )
 }
 
-function Appearance({ title, description, event, cta, href }) {
-  return (
-    <Card as="article">
-      <Card.Title as="h3" href={href}>
-        {title}
-      </Card.Title>
-      <Card.Eyebrow decorate>{event}</Card.Eyebrow>
-      <Card.Description>{description}</Card.Description>
-      <Card.Cta>{cta}</Card.Cta>
-    </Card>
-  )
+function MailIcon(props) {
+    return (
+        <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+            <path
+                fillRule="evenodd"
+                d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"
+            />
+        </svg>
+    )
 }
 
-export default function Speaking() {
-  return (
-    <>
-      <Head>
-        <title>Speaking - Levi</title>
-        <meta
-          name="description"
-          content="I’ve spoken at events all around the world and been interviewed for many podcasts."
-        />
-      </Head>
-      <SimpleLayout>
-        <div className="space-y-20">
+export default function About() {
+    return (
+        <>
+            <Head>
+                <title>About - Levi</title>
+                <meta
+                    name="description"
+                    content="I’m Levi. I live in VietNam, where I design the future."
+                />
+            </Head>
+            <Container className="mt-16 sm:mt-32">
+                <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
+                    <div className="lg:pl-20">
+                        <div className="max-w-xs px-2.5 lg:max-w-none">
+                            <Image
+                                src={sadImage}
+                                alt=""
+                                sizes="(min-width: 1024px) 32rem, 20rem"
+                                className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
+                            />
+                        </div>
+                    </div>
+                    <div className="lg:order-first lg:row-span-2">
+                        <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
 
+                        </h1>
+                        <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+                            <p>"The light breeze came and I thought it was spring</p>
+                            <p>The sun rose and I thought winter had passed</p>
+                                <p>Who would have thought that winter is still here</p>
+                                <p>Spring has not yet arrived..."</p>
 
-
-        </div>
-      </SimpleLayout>
-    </>
-  )
+                        </div>
+                    </div>
+                  </div>
+            </Container>
+        </>
+    )
 }
