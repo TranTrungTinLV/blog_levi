ef902976c69e63f1dc005b0c6ff5634cae407bd2
Aldor Tran <aldor.tran@gmail.com>
2023-03-30 00:39:03 +0700

    first blog levi

---
+++ src/pages/speaking.jsx
@@ -0,0 +1,47 @@
+import Head from 'next/head'
+
+import { Card } from '@/components/Card'
+import { Section } from '@/components/Section'
+import { SimpleLayout } from '@/components/SimpleLayout'
+
+function SpeakingSection({ children, ...props }) {
+  return (
+    <Section {...props}>
+      <div className="space-y-16">{children}</div>
+    </Section>
+  )
+}
+
+function Appearance({ title, description, event, cta, href }) {
+  return (
+    <Card as="article">
+      <Card.Title as="h3" href={href}>
+        {title}
+      </Card.Title>
+      <Card.Eyebrow decorate>{event}</Card.Eyebrow>
+      <Card.Description>{description}</Card.Description>
+      <Card.Cta>{cta}</Card.Cta>
+    </Card>
+  )
+}
+
+export default function Speaking() {
+  return (
+    <>
+      <Head>
+        <title>Speaking - Levi</title>
+        <meta
+          name="description"
+          content="I’ve spoken at events all around the world and been interviewed for many podcasts."
+        />
+      </Head>
+      <SimpleLayout>
+        <div className="space-y-20">
+
+
+
+        </div>
+      </SimpleLayout>
+    </>
+  )
+}
---
+++ src/pages/uses.jsx
@@ -0,0 +1,48 @@
+import Head from 'next/head'
+
+import { Card } from '@/components/Card'
+import { Section } from '@/components/Section'
+import { SimpleLayout } from '@/components/SimpleLayout'
+
+function ToolsSection({ children, ...props }) {
+  return (
+    <Section {...props}>
+      <ul role="list" className="space-y-16">
+        {children}
+      </ul>
+    </Section>
+  )
+}
+
+function Tool({ title, href, children }) {
+  return (
+    <Card as="li">
+      <Card.Title as="h3" href={href}>
+        {title}
+      </Card.Title>
+      <Card.Description>{children}</Card.Description>
+    </Card>
+  )
+}
+
+export default function Uses() {
+  return (
+    <>
+      <Head>
+        <title>Uses - Levi</title>
+        <meta
+          name="description"
+          content="Software I use, gadgets I love, and other things I recommend."
+        />
+      </Head>
+
+
+
+
+
+
+
+
+    </>
+  )
+}
